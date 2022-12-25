c0edcd4b20c476dd0353688f56da4839b7419bcd
Sammy Samkough <sammysamkough@hotmail.com>
2022-12-25 10:29:34 -0500

    weird relative pathing stuff

--- webserver.ts
+++ webserver.ts
@@ -3,13 +3,25 @@ import type { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 const router = new Router();
 router
-  .get("/", async (context: Context) => {
-    const html_file = await Deno.readFile("./public/index.html");
-    context.response.body = html_file;
+  .get("/", async (context: Context, next) => {
+    try {
+      await context.send({
+        root: `${Deno.cwd()}/public/`,
+        index: "index.html",
+      });
+    } catch {
+      await next();
+    }
   })
-  .get("/about", async (context: Context) => {
-    const html_file = await Deno.readFile("./public/about/index.html");
-    context.response.body = html_file;
+  .get("/about", async (context: Context, next) => {
+    try {
+      await context.send({
+        root: `${Deno.cwd()}/public/about/`,
+        index: "index.html",
+      });
+    } catch {
+      await next();
+    }
   });
 
 const app = new Application();
@@ -17,8 +29,8 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(async (context) => {
   await context.send({
-    root: `${Deno.cwd()}/`,
-    index: "public/index.html",
+    root: `${Deno.cwd()}/public/`,
+    index: "index.html",
   });
 });
 
